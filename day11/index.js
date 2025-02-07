4791685004bf1cd121a5f761627fd71b3c2668eb
CRE4MMM <haidariqq90@gmail.com>
2025-02-07 14:37:33 +0700

    add file day 11

---
+++ day11/index.js
@@ -0,0 +1,6 @@
+var rock = "rock";
+var paper = "paper";
+var scissor = "scissor";
+var pilih = prompt("1. rock, 2. paper, 3. scissor");
+function winner() {
+}
---
+++ day11/index.ts
@@ -0,0 +1,8 @@
+const rock: string = "rock";
+const paper: string = "paper";
+const scissor: string = "scissor"
+const pilih: string | null = prompt("1. rock, 2. paper, 3. scissor")
+
+function winner () {
+    
+}
