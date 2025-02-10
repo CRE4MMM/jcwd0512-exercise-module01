804f8b32e140aa8f9e6558a117cb8d8c2e3d95af
CRE4MMM <haidariqq90@gmail.com>
2025-02-10 14:22:23 +0700

    update

--- day11/index.js
+++ day11/index.js
@@ -1,6 +1,43 @@
 var rock = "rock";
 var paper = "paper";
 var scissor = "scissor";
-var pilih = prompt("1. rock, 2. paper, 3. scissor");
-function winner() {
+function npc() {
+    var choices = [rock, paper, scissor];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+function winner(player, computer) {
+    if (player === computer) {
+        return "It's a tie!";
+    }
+    if ((player === rock && computer === scissor) ||
+        (player === paper && computer === rock) ||
+        (player === scissor && computer === paper)) {
+        return "You win!";
+    }
+    else {
+        return "You lose!";
+    }
+}
+var pilih = prompt("Choose: 1. rock, 2. paper, 3. scissor");
+if (pilih !== null) {
+    var playerChoice = void 0;
+    switch (pilih) {
+        case "1":
+            playerChoice = rock;
+            break;
+        case "2":
+            playerChoice = paper;
+            break;
+        case "3":
+            playerChoice = scissor;
+            break;
+        default:
+            alert("Invalid choice!");
+            throw new Error("Invalid choice");
+    }
+    var computerChoice = npc();
+    alert("Computer chose: ".concat(computerChoice, "\n").concat(winner(playerChoice, computerChoice)));
+}
+else {
+    alert("Game canceled");
 }
--- day11/index.ts
+++ day11/index.ts
@@ -1,8 +1,47 @@
 const rock: string = "rock";
 const paper: string = "paper";
-const scissor: string = "scissor"
-const pilih: string | null = prompt("1. rock, 2. paper, 3. scissor")
+const scissor: string = "scissor";
 
-function winner () {
-    
+function npc(): string {
+    const choices = [rock, paper, scissor];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function winner(player: string, computer: string): string {
+    if (player === computer) {
+        return "It's a tie!";
+    }
+    if (
+        (player === rock && computer === scissor) ||
+        (player === paper && computer === rock) ||
+        (player === scissor && computer === paper)
+    ) {
+        return "You win!";
+    } else {
+        return "You lose!";
+    }
+}
+
+const pilih: string | null = prompt("Choose: 1. rock, 2. paper, 3. scissor");
+if (pilih !== null) {
+    let playerChoice: string;
+    switch (pilih) {
+        case "1":
+            playerChoice = rock;
+            break;
+        case "2":
+            playerChoice = paper;
+            break;
+        case "3":
+            playerChoice = scissor;
+            break;
+        default:
+            alert("Invalid choice!");
+            throw new Error("Invalid choice");
+    }
+
+    const computerChoice = npc();
+    alert(`Computer chose: ${computerChoice}\n${winner(playerChoice, computerChoice)}`);
+} else {
+    alert("Game canceled");
 }
